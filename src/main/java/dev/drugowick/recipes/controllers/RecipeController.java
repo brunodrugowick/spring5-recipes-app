5c7ca71bde8785d799ba0756dda49873369d9753
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-05-11 03:53:32 -0300

    Adding basic post capability.

--- src/main/java/dev/drugowick/recipes/controllers/RecipeController.java
+++ src/main/java/dev/drugowick/recipes/controllers/RecipeController.java
@@ -1,10 +1,14 @@
 package dev.drugowick.recipes.controllers;
 
+import dev.drugowick.recipes.converters.commands.RecipeCommand;
 import dev.drugowick.recipes.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RecipeController {
@@ -19,6 +23,19 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping(name = "recipe", method = RequestMethod.POST)
+    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
