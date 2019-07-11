e290ec3784947e16b68cf638955770d14e68098c
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-07-10 21:51:20 -0300

    Adds image exhibition and upload to recipes.

---
+++ src/main/java/dev/drugowick/recipes/controllers/ImageController.java
@@ -0,0 +1,65 @@
+package dev.drugowick.recipes.controllers;
+
+import dev.drugowick.recipes.converters.commands.RecipeCommand;
+import dev.drugowick.recipes.services.ImageService;
+import dev.drugowick.recipes.services.RecipeService;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.ContentTypeUtils;
+
+@Controller
+public class ImageController {
+
+    private final RecipeService recipeService;
+    private final ImageService imageService;
+
+    public ImageController(RecipeService recipeService, ImageService imageService) {
+        this.recipeService = recipeService;
+        this.imageService = imageService;
+    }
+
+    @GetMapping("recipe/{id}/image")
+    public String showUploadForm(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return "recipe/imageuploadform";
+    }
+
+    @PostMapping("recipe/{id}/image")
+    public String handleImagePost(@PathVariable String id, @RequestParam("imagefile")MultipartFile file){
+        imageService.saveImageFile(Long.parseLong(id), file);
+        return "redirect:/recipe/" + id + "/show";
+    }
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderRecipeImage(@PathVariable String id, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+        response.setContentType("image/jpeg");
+
+        if (recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+            int i = 0;
+            for (Byte wrappedByte : recipeCommand.getImage()) {
+                byteArray[i++] = wrappedByte; //auto unboxing
+            }
+
+            InputStream inputStream = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(inputStream, response.getOutputStream());
+        } else {
+            //todo set a default image?
+            IOUtils.copy(null, response.getOutputStream());
+        }
+    }
+}
---
+++ src/main/java/dev/drugowick/recipes/services/ImageServiceImpl.java
@@ -0,0 +1,41 @@
+package dev.drugowick.recipes.services;
+
+import dev.drugowick.recipes.domain.Recipe;
+import dev.drugowick.recipes.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()){
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            log.error("Error occurred", e);
+            e.printStackTrace();
+        }
+    }
+}
